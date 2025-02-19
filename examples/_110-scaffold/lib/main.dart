2562e2f171a05416adc015120749370b73d5e820
PeterKassenaar <info@kassenaar.com>
2025-02-19 11:12:03 +0100

    fix: update examples to flutter 3.7

--- examples/_110-scaffold/lib/main.dart
+++ examples/_110-scaffold/lib/main.dart
@@ -11,6 +11,7 @@ void main() {
             child: Text('This is my Flutter app')
         ),
           floatingActionButton: FloatingActionButton(
+            onPressed: (){}, // Empty function (for now), as otherwise the application won't compile! 'onPressed()' is required for FAB.
             child: Text('click'),
           ),
       )
