a66d34d29b22e0d2d082e5aa8763e3ec0301c43e
PeterKassenaar <info@kassenaar.com>
2025-03-06 13:50:32 +0100

    chore: some widget updates

--- examples/_160-containers-padding/lib/screens/home.dart
+++ examples/_160-containers-padding/lib/screens/home.dart
@@ -12,15 +12,16 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[800],
       ),
       // 1. Container Widget. A container can have a child and multiple properties
-      // body: Container(
-      //   // The Container has a child, so the container will
-      //   // only fill the childs background
-      //   color: Colors.green[200],
-      //   child: Text('Hello Container'),
-      //   // Giving the container some margin and padding
-      //   margin: EdgeInsets.all(20.0),
-      //   padding: EdgeInsets.all(20.0),
-      // ),
+      body: Container(
+        // The Container HAS a child, so the container will
+        // ONLY fill the childs background!
+        color: Colors.green[200],
+        child: Text('Hello Container'),
+        // Giving the container some margin and padding
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(20.0),
+      ),
+      // backgroundColor: Colors.blue[800], // uncomment if you want the complete Scaffold() widget to have a certain background color
       // 2. Padding as a 'standalone' widget
       // We can only use padding and child as properties. Nothing else.
       // body: Padding(
@@ -29,18 +30,18 @@ class Home extends StatelessWidget {
       // ),
       // 3. If we need some layout (like, in this case, a border), use
       // the Container() widget and `padding` inside the container
-      body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.red, // Border color
-            width: 1.0, // Border width
-          ),
-          borderRadius: BorderRadius.circular(8.0), // Optional: Rounded corners
-        ),
-        margin: EdgeInsets.all(40.0),
-        padding: EdgeInsets.all(20.0), // Add some padding inside the border
-        child: Text('Some text in a Padding() widget'),
-      ),
+      // body: Container(
+      //   decoration: BoxDecoration(
+      //     border: Border.all(
+      //       color: Colors.red, // Border color
+      //       width: 1.0, // Border width
+      //     ),
+      //     borderRadius: BorderRadius.circular(8.0), // Optional: Rounded corners
+      //   ),
+      //   margin: EdgeInsets.all(40.0),
+      //   padding: EdgeInsets.all(20.0), // Add some padding inside the border
+      //   child: Text('Some text in a Padding() widget'),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.green[800],
