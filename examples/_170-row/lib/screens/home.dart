ba2961fab3b9a052256dd1c2f431f0107d49e5fd
PeterKassenaar <info@kassenaar.com>
2025-03-06 14:10:05 +0100

    fix: update to Flutter 3.7

--- examples/_170-row/lib/screens/home.dart
+++ examples/_170-row/lib/screens/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 // Our own class, extending the base StatelessWidget class
 class Home extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -11,22 +10,31 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[800],
       ),
-      // 1. Row Widget. A Row can have an array of children. They are all widgets
+      // 1. Row Widget. A Row can have an ARRAY of children.
+      // All children are widgets.
       body: Row(
         // 2. experiment with the alignment settings below
+        // Details on axis alignment: https://api.flutter.dev/flutter/rendering/MainAxisAlignment.html and more.
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget> [
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
           // 3. experiment with more/less children in the <Widget>[]
+          //..child 1
           FlutterLogo(),
+
+          //..child 2
           Text('Hello World'),
+
+          //..child 3
           ElevatedButton(
               onPressed: (){},
               child: Text('Click me')
           ),
+
+          //..child 4
           Container(
             padding: EdgeInsets.all(20.0),
-            child: Text('Container'),
+            child: Text('Container in the row'),
             color: Colors.green[200],
           )
         ],
