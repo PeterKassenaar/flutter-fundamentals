d9f838bcaccb86907c2e2a05f913d9d0755ec6c3
PeterKassenaar <info@kassenaar.com>
2025-03-07 10:35:18 +0100

    feat: add _195-sample-layouts

--- examples/_195-sample-layouts/lib/screens/PuppyList.dart
+++ examples/_195-sample-layouts/lib/screens/PuppyList.dart
@@ -4,6 +4,11 @@ class PuppyList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // The array containing the puppy-details.
+    // They are objects of <String, String> (e.g. "name": "Joakim"), wrapped
+    // in a Map() inside a List(). More info:
+    //  - https://api.flutter.dev/flutter/dart-core/Map/Map.html
+    //  - https://api.flutter.dev/flutter/dart-core/List-class.html
     final List<Map<String, String>> puppies = [
       {"name": "Joakim", "desc": "Joakim is a nice fellow currently looking for a new home.", "image": "assets/puppy-1.jpg"},
       {"name": "Tommy", "desc": "Tommy loves balls, sticks, socks and your slippers.", "image": "assets/puppy-2.jpg"},
@@ -12,38 +17,46 @@ class PuppyList extends StatelessWidget {
       {"name": "Hannes", "desc": "Hannes is a beast, but a cute one. You'll never want to get in trouble with him.", "image": "assets/puppy-5.jpg"},
     ];
 
+    // The default Scaffold() widget, containing all content
     return Scaffold(
       appBar: AppBar(
         title: Text('The Puppy List', style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: puppies.length,
-        itemBuilder: (context, index) {
-          final puppy = puppies[index];
-          return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                puppy["image"]!,
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
+      // the `body` is of type ListView()
+      // See https://api.flutter.dev/flutter/widgets/ListView-class.html for details
+      body: Container(
+        color: Colors.brown[200],
+        child: ListView.builder(
+          itemCount: puppies.length,
+          itemBuilder: (context, index) {
+            final puppy = puppies[index];
+            return ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  puppy["image"]!,
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            title: Text(
-              puppy["name"]!,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(puppy["desc"]!),
-            trailing: Icon(Icons.more_vert),
-          );
-        },
+              title: Text(
+                puppy["name"]!,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(puppy["desc"]!),
+              trailing: Icon(Icons.more_vert),
+            );
+          },
+        ),
       ),
+      // The bar with icons at the bottom of the screen.
+      // See https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html for details.
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.brown[300],
-        selectedItemColor: Colors.blue,
+        backgroundColor: Colors.brown[400],
+        selectedItemColor: Colors.white,
         unselectedItemColor: Colors.black54,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
