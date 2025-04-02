2a107000792e1583989695a1977d4ce3819e0e42
PeterKassenaar <info@kassenaar.com>
2025-04-02 12:56:17 +0200

    feat: updated FAB-example documentation

--- examples/_110-scaffold/lib/main.dart
+++ examples/_110-scaffold/lib/main.dart
@@ -7,11 +7,10 @@ void main() {
           title: new Text('Hello Flutter'),
             centerTitle: true,
           ),
-          body: Center(
-            child: Text('This is my Flutter app')
-        ),
+          body: Center(child: Text('This is my Flutter app')),
           floatingActionButton: FloatingActionButton(
             onPressed: (){}, // Empty function (for now), as otherwise the application won't compile! 'onPressed()' is required for FAB.
+            // if you place 'null' as its onPressed: property, the button is also disabled!
             child: Text('click'),
           ),
       )
